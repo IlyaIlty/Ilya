bbba2c81ed6e776a330a1ec91ab3215538111269
IlyaIlty <yrchenkovilya@yandex.ru>
2025-07-23 23:27:01 +0300

    Дабавлена кнопка выкл музыки

--- Remainder.py
+++ Remainder.py
@@ -6,6 +6,7 @@ import time
 import pygame
 
 t = 0
+music = False
 
 def set():
     global t
@@ -35,9 +36,19 @@ def check():
 
 
 def play_snd():
+    global mosic
+    music = True
     pygame.mixer.init()
     pygame.mixer.music.load('reminder.mp3')
     pygame.mixer.music.play()
+
+
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text='Установить новое напоминание')
 
 
 window = Tk()
@@ -46,6 +57,9 @@ label = Label(text='Установите напоминание', font=('Arial',
 label.pack(pady=10)
 set_button = Button(text='Установить напоминаине', command=set)
 set_button.pack()
+
+stop_button = Button('Остановить музыку', command=stop_music)
+stop_button.pack(pady=10)
 
 check()
 
